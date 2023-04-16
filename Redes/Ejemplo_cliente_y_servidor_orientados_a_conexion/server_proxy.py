29faba559ed7d60da5115e6d89882dd3f7669aef
Franzo1 <franzwiderstrom1@outlook.com>
2023-04-16 14:57:17 -0400

    Avance mediodia 16 04

--- Redes/Ejemplo_cliente_y_servidor_orientados_a_conexion/server_proxy.py
+++ Redes/Ejemplo_cliente_y_servidor_orientados_a_conexion/server_proxy.py
@@ -1,7 +1,5 @@
 import socket
-import requests
 import json
-import httplib2
 
 def receive_http(connection_socket, buff_size, end_head):
     head_list = list()
@@ -17,13 +15,6 @@ def receive_http(connection_socket, buff_size, end_head):
         recv_http = connection_socket.recv(buff_size)
         full_head += recv_http
         is_end_of_head = end_head in full_head.decode()
-
-    """ head_decode = full_head.decode()
-    after_head = head_decode.split(end_head, 1)[1]
-    print(head_decode)
-    content_length = (head_decode.split("Content-Length: ", 1)[1]).split("\r\n", 1)[0]
-    body_list = receive_body(connection_socket, buff_size, int(content_length), after_head)
-    head_list.append((recv_http.decode()).split(end_head, 1)[0] + end_head) """
 
     head_list.append(recv_http.decode())
 
@@ -48,26 +39,31 @@ def receive_head(connection_socket, buff_size, end_head):
     content_length = int((head_decode.split("Content-Length: ", 1)[1]).split("\r\n", 1)[0])
     after_head = head_decode.split(end_head, 1)[1]
     head_decode = head_decode.split(end_head, 1)[0] + end_head
-    body_list = receive_body(connection_socket, buff_size, int(content_length), after_head)
+    body_string = receive_body(connection_socket, buff_size, int(content_length), after_head)
     head_list.append(head_decode)
 
-    return head_list, body_list
+    return head_list, body_string
 
 
 def receive_body(connection_socket, buff_size, content_length, body_start):
     body_list = list()
-    body_list.append(body_start)
+    bytes_body = body_start.encode()
     num_bytes = len(body_start.encode('utf-8'))
 
     while num_bytes < content_length:
         recv_body = connection_socket.recv(buff_size)
-        body_list.append(recv_body.decode())
+        body_list.append(recv_body)
         num_bytes += buff_size
 
-    return body_list
+    for i in body_list:
+        bytes_body += i
+
+    string_body = bytes_body.decode()
+
+    return string_body
 
 
-def send_http(head_list):
+def send_http(head_list, buffer_size):
     if ("GET" in head_list[0]):
         http_string = ''.join(head_list)
         adressRequest = (http_string.split("GET ", 1)[1].split(" HTTP", 1))[0].replace(" ", "")
@@ -75,13 +71,10 @@ def send_http(head_list):
             data = json.load(file)
             blocked_list = data['blocked']
         if adressRequest in blocked_list:
-            return "HTTP/1.1 403 Forbidden\r\n\r\n"
+            return 'HTTP/1.1 403 Forbidden\r\n\r\n<html><head><title>403 Forbidden</title></head><body bgcolor="white"><center><h1>403 Forbidden</h1></center></body></html>'
         else:
             adressHost = (http_string.split("Host: ", 1)[1].split("\r\n", 1))[0].replace(" ", "")
-            # host = (http_string.split("Host: ", 1)[1].split("\r\n", 1))[0].replace(" ", "")
-            # response = requests.get("http://example.com")
             client_proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # ip = socket.gethostbyname("example.com")
             address = (adressHost, 80)
             client_proxy.connect(address)
             with open("/home/franz/Escritorio/RedesGit/Redes/Redes/Ejemplo_cliente_y_servidor_orientados_a_conexion/name_user.json") as file:
@@ -91,32 +84,23 @@ def send_http(head_list):
             http_string += "X-ElQuePregunta: "+name_user+"\r\n\r\n"
             print(http_string)
             client_proxy.send(http_string.encode())
-            HEAD, BODY = receive_head(client_proxy, 1024, "\r\n\r\n")
+            HEAD, BODY = receive_head(client_proxy, buffer_size, "\r\n\r\n")
             head_string = ''.join(HEAD)
-            print(head_string)
-            body_string = ''.join(BODY)
             forbidden_list = data["forbidden_words"]
             for i in forbidden_list:
                 word = list(i.keys())[0]
-                if word in body_string:
-                    body_string = body_string.replace(word,i[word])
-
-            """ dict = list(response.headers.keys())
-            HEAD = ""
-            HEAD += (http_string.split(" ", 2)[2].split("\r\n", 1))[0].replace(" ", "")
-            HEAD += " " + str(response.status_code) + " " + response.reason + "\r\n"
-            for keys in dict:
-                HEAD += keys + ": " + response.headers[keys] + "\r\n" """
+                if word in BODY:
+                    BODY = BODY.replace(word,i[word])
             head_string = head_string[:-2]
             head_string += "X-ElQuePregunta: "+name_user+"\r\n\r\n" 
-            HTTP = head_string + body_string
+            HTTP = head_string + BODY
             return HTTP
 
 
 
-buff_size = 50
+buff_size = 5000
 end_of_head = "\r\n\r\n"
-new_socket_address = ('localhost', 8001)
+new_socket_address = ('localhost', 8000)
 
 print('Creando socket - Proxy')
 
@@ -133,7 +117,8 @@ while True:
 
     print('Se ha recibido el HTTP')
 
-    response = send_http(recv_http)
+    response = send_http(recv_http, buff_size)
+    print(response)
     new_socket.send(response.encode())
 
     print('Se ha recibido la response')
